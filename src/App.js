b882277aaa9de15cd4165070748eef5e8f9ff54b
CodeSnob98 <mukherjeebiswarup112233@gmail.com>
2022-07-26 22:46:11 +0530

    Added initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
+import Comment from "./components/Comment";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [showableData, setShowableData] = useState([]);
+  const ref = useRef("");
+
+  function addComment() {
+    data.push({
+      id: Date.now(),
+      comment: ref.current.value,
+      reply: [],
+      level: 0,
+      disabled: false,
+    });
+    setData(data);
+    settingShowableData(data);
+    ref.current.value = "";
+  }
+
+  function replyComment(id, com) {
+    function findNReply(id, com, data) {
+      data.forEach((el) => {
+        if (el.id === id) {
+          el.reply.push({
+            id: Date.now(),
+            comment: com,
+            reply: [],
+            disabled: false,
+          });
+          return;
+        } else if (el.reply.length > 0) {
+          return findNReply(id, com, el.reply);
+        }
+      });
+    }
+    findNReply(id, com, data);
+    setData(JSON.parse(JSON.stringify(data)));
+    settingShowableData(JSON.parse(JSON.stringify(data)));
+  }
+
+  function deleteComment(id) {
+    function findNDelete(id, data) {
+      data.forEach((el) => {
+        if (el.id === id) {
+          el.disabled = true;
+          return;
+        } else if (el.reply.length > 0) {
+          return findNDelete(id, el.reply);
+        }
+      });
+    }
+    findNDelete(id, data);
+    setData(JSON.parse(JSON.stringify(data)));
+    settingShowableData(JSON.parse(JSON.stringify(data)));
+  }
+
+  function editComment(id, com) {
+    function findNEdit(id, com, data) {
+      data.forEach((el) => {
+        if (el.id === id) {
+          el.comment = com;
+          return;
+        } else if (el.reply.length > 0) {
+          return findNEdit(id, com, el.reply);
+        }
+      });
+    }
+    findNEdit(id, com, data);
+    setData(JSON.parse(JSON.stringify(data)));
+    settingShowableData(JSON.parse(JSON.stringify(data)));
+  }
+
+  function settingShowableData(data) {
+    let myData = flattenData(data, 0);
+    setShowableData(myData);
+  }
+
+  function flattenData(data, i) {
+    let myData = [];
+    data.forEach((el) => {
+      if (!el.disabled) {
+        el.level = i;
+        myData.push(el);
+        if (el.reply.length > 0) {
+          myData.push(...flattenData(el.reply, i + 1));
+        }
+      }
+    });
+    return myData;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display-row">
+        <input ref={ref} type="text"></input>
+        <button className="add-reply-com" onClick={addComment}>
+          Add comment
+        </button>
+      </div>
+      <div className="display-col">
+        {showableData.map((e) => {
+          if (!e.disabled) {
+            return (
+              <div
+                style={{
+                  marginLeft: `${20 * e.level}px`,
+                  borderLeft: "1px solid green",
+                }}
+                key={e.id}
+              >
+                <Comment
+                  data={e}
+                  reply={replyComment}
+                  edit={editComment}
+                  delete={deleteComment}
+                />
+              </div>
+            );
+          }
+          return <></>;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Comment.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+export default function Comment(props) {
+  const [showIp, setShowIp] = useState(false);
+  const [showComment, setShowComment] = useState(true);
+  const [myVal, setMyVal] = useState("");
+  const [replyClicked, setReplyClicked] = useState(false);
+  const [editClicked, setEditClicked] = useState(false);
+  function replyToComment() {
+    if (!showIp) {
+      setShowIp(true);
+      setReplyClicked(true);
+    } else {
+      if (myVal) {
+        props.reply(props.data.id, myVal);
+        setShowIp(false);
+        setReplyClicked(false);
+      } else {
+        alert("can send empty text");
+      }
+    }
+  }
+
+  function editComment() {
+    if (!showIp) {
+      setShowIp(true);
+      setShowComment(false);
+      setMyVal(props.data.comment);
+      setEditClicked(true);
+    } else {
+      if (myVal) {
+        props.edit(props.data.id, myVal);
+        setShowIp(false);
+        setShowComment(true);
+        setEditClicked(false);
+        setMyVal("");
+      } else {
+        alert("can set empty text");
+      }
+    }
+  }
+  function deletComment() {
+    props.delete(props.data.id);
+  }
+  function cancel() {
+    setShowIp(false);
+    setShowComment(true);
+  }
+  return (
+    <div className="display-row">
+      {showComment && <p>{props.data.comment}</p>}
+      {showIp && (
+        <input
+          type="text"
+          onChange={(e) => {
+            setMyVal(e.target.value);
+          }}
+          value={myVal}
+        />
+      )}
+      <button className="delete-com" onClick={deletComment}>
+        Delete
+      </button>
+
+      {!editClicked && (
+        <button className="add-reply-com" onClick={replyToComment}>
+          Reply
+        </button>
+      )}
+
+      {!replyClicked && (
+        <button className="add-reply-com" onClick={editComment}>
+          Edit
+        </button>
+      )}
+
+      {showIp && (
+        <button className="add-reply-com" onClick={cancel}>
+          Cancel
+        </button>
+      )}
+    </div>
+  );
+}
